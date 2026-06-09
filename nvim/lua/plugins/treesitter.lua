0a1223306d2a91b29f2f55ab80449f112a2a4256
Tim Tattersall <tim@tattsy.com>
2026-06-09 21:51:14 +1000

    feat: use tree-sitter-manager plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,37 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'scss', 'bash', 'css', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'yaml', 'terraform', 'norg' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+    'romus204/tree-sitter-manager.nvim',
+    dependencies = {},
+    config = function()
+      require('tree-sitter-manager').setup {
+        ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'html',
+          'javascript',
+          'jsdoc',
+          'json',
+          'lua',
+          'go',
+          'luadoc',
+          'luap',
+          'markdown',
+          'markdown_inline',
+          'printf',
+          'python',
+          'query',
+          'regex',
+          'toml',
+          'tsx',
+          'typescript',
+          'vim',
+          'vimdoc',
+          'xml',
+          'yaml',
+        },
+        auto_install = true,
+      }
 
       vim.treesitter.language.register('terraform', { 'terraform', 'terraform-vars' })
     end,
